2d05b4a5b453612137d761508a4ba82aa3e6c4ce
Tonia Tong <tonia@Tonias-MacBook-Pro.local>
2018-07-07 15:23:01 -0400

    Added a temporary default screen to the home page.

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Text, View } from 'react-native'
+
+const StyledView = styled(View)`
+    align-items: center;
+    background-color: #245d8c;
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+    justify-content: center;
+    width: 100%;
+`
+
+const StyledText = styled(Text)`
+    color: #ffffff;
+    font-size: 18px;
+`
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Hello, welcome to the China Bridge App!</Text>
-      </View>
+      <StyledView>
+        <StyledText>Welcome to the China Bridge App!</StyledText>
+      </StyledView>
     )
   }
 }
