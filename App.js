679d38886f2f7aec9998e726c70d05e5f69f9907
billyhsun <39208126+billyhsun@users.noreply.github.com>
2018-10-11 23:40:07 -0400

    Added folders

--- App.js
+++ App.js
@@ -12,25 +12,25 @@ import {
   createStackNavigator,
 } from 'react-navigation';
 
-import Splash_Screen from './Splash_Screen';
-import MainPage from './MainPage';
-import MenuPage from './MenuPage';
-import WebPage from './WebView';
-import AboutPage from './About';
-import AnnouncementPage from './Announcements';
-import ContactUs from './ContactPage';
-import SettingsPage from './Settings';
-import SpeakerMain from './Speaker_main';
-import gen_speaker_page from './Speaker_individual';
-import SpeakerJH from './SpeakerJH';
-import SpeakerMD from './SpeakerMD';
-import SpeakerJT from './SpeakerJT';
-import SpeakerPW from './SpeakerPW';
-import SpeakerLP from './SpeakerLP';
-import SeriesMenu from './Series_main';
-import Series_1 from './Series1';
-import gen_sermon_page from './Sermon_individual';
-import manage_downloads from './Downloads';
+import Splash_Screen from './views/main/Splash_Screen';
+import MainPage from './views/main/MainPage';
+import MenuPage from './views/main/MenuPage';
+import WebPage from './views/main/WebView';
+import AboutPage from './views/main/About';
+import AnnouncementPage from './views/announcements/Announcements';
+import ContactUs from './views/main/ContactPage';
+import SettingsPage from './views/settings/Settings';
+import SpeakerMain from './views/speakers/Speaker_main';
+import gen_speaker_page from './views/speakers/Speaker_individual';
+import SpeakerJH from './views/speakers/SpeakerJH';
+import SpeakerMD from './views/speakers/SpeakerMD';
+import SpeakerJT from './views/speakers/SpeakerJT';
+import SpeakerPW from './views/speakers/SpeakerPW';
+import SpeakerLP from './views/speakers/SpeakerLP';
+import SeriesMenu from './views/series/Series_main';
+import Series_1 from './views/series/Series1';
+import gen_sermon_page from './views/series/Sermon_individual';
+import manage_downloads from './views/series/Downloads';
 
 
 const App = createStackNavigator(
